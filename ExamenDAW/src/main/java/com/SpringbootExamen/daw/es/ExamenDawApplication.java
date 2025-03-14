2bdd547e59e70bc3c65a8ad0107c53a0587bac3b
Helena Peño Carrera <helenapenocarrera@MacBook-Pro-de-Helena.local>
2025-03-14 20:10:08 +0100

    prueba examen subida

---
+++ ExamenDAW/src/main/java/com/SpringbootExamen/daw/es/ExamenDawApplication.java
@@ -0,0 +1,13 @@
+package com.SpringbootExamen.daw.es;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenDawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenDawApplication.class, args);
+	}
+
+}
